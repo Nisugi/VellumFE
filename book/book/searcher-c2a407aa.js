a4b5cbcf30f8adce3362a4d623c8f6fe45a4ce22
Nisugi <nisugi-gs4@gmail.com>
2026-07-26 04:01:46 -0500

    docs: link the VellumFE Discord (README badge + community section, book intro + FAQ)

--- book/book/searcher-c2a407aa.js
+++ book/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ebfe9943.js',
+                path_to_root + 'searchindex-13ea9d77.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
